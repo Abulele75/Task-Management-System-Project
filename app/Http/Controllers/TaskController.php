253b875cffc2d8454c1d87de901381b10e8750ab
AgcobileGqabaza <agcobilegqabaza10@gmail.com>
2026-06-06 14:34:36 +0200

    Add TaskController

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Task;
+use App\Models\AAUser;
+
+class TaskController extends Controller
+{
+    public function index() {
+        $tasks = Task::where('user_id', Auth::id())->get();
+        return view('tasks.index', compact('tasks'));
+    }
+
+    public function create() {
+        return view('tasks.create');
+    }
+
+    public function store(Request $request) {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'priority' => 'required',
+            'status' => 'required',
+            'deadline' => 'required|date',
+        ]);
+
+        Task::create([
+
+            'title' => $request->title,
+            'description' => $request->description,
+            'priority' => $request->priority,
+            'status' => $request->status,
+            'deadline' => $request->deadline,
+            'user_id' => Auth::id(),
+        ]);
+
+        return redirect()->route('tasks.index');
+    }
+    public function edit(Task $task) {
+    return view('tasks.edit', compact('task'));
+}
+
+public function update(Request $request, Task $task) {
+    $request->validate([
+        'title' => 'required|string|max:255',
+        'priority' => 'required',
+        'status' => 'required',
+        'deadline' => 'required|date',
+    ]);
+
+    $task->update([
+        'title' => $request->title,
+        'description' => $request->description,
+        'priority' => $request->priority,
+        'status' => $request->status,
+        'deadline' => $request->deadline,
+    ]);
+
+    return redirect()->route('tasks.index');
+}
+public function destroy(Task $task) {
+    $task->delete();
+    return redirect()->route('tasks.index');
+}
+}
