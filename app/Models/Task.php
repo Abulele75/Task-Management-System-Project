f414d0356dd43d087ca392938bd64fe961033e90
AgcobileGqabaza <agcobilegqabaza10@gmail.com>
2026-06-06 15:04:45 +0200

    Add category relationship to Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -13,9 +13,14 @@ class Task extends Model
         'priority',
         'status',
         'deadline',
-        'user_id'
+        'user_id',
+        'category_id'
     ];
 
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+        
     public function user() {
         return $this->belongsTo(AAUser::class);
     }
